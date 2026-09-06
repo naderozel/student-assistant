201c4d1f44271a9c6dd6eb9b632fc102a31c8ec8
naderozel <34371437+naderozel@users.noreply.github.com>
2026-09-06 16:17:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.title("AI Student Assistant")
 
 st.sidebar.title("Tools")
 
-tool = st.sidebar.radio("Choose a tool:", [" Chatbot", "📄PDF Summarize", "📸image editor", "🌐Translator", "🎙️ Extract Text from Audio, Video & YouTube"])
+tool = st.sidebar.radio("Choose a tool:", [" Chatbot", "📄PDF Summarize", "📸image editor", "🌐Translator", "🎙️ Extract Text from Audio, Video & YouTube", "🎙️ Audio to Text"])
 
 if tool == " Chatbot":
     st.header("Chatbot")
@@ -407,6 +407,46 @@ elif tool == "🎙️ Extract Text from Audio, Video & YouTube":
                 except Exception as e:
                     st.error(f"Error: {e}")
 
+elif tool == "🎙️ audio to text":
+    st.header("🎙️ Audio to Text")
+    text = st.text_area("Write text here")
+
+    lang = st. selectbox("Choose language", ["عربي", "English"])
+
+    voices = {
+        "عربي": {
+            
+            " زارية (امرأة - السعودية)": "ar-SA-ZariyahNeural",
+            " شاكر (رجل - مصر)":         "ar-EG-ShakirNeural",
+            " سلمى (امرأة - مصر)":       "ar-EG-SalmaNeural",
+        },
+        "English": {
+            " غاي (رجل - أمريكا)":       "en-US-GuyNeural",
+            " جيني (امرأة - أمريكا)":    "en-US-JennyNeural",
+            " رايان (رجل - بريطانيا)":   "en-GB-RyanNeural",
+            " ليبي (امرأة - بريطانيا)": "en-GB-LibbyNeural",
+        },
+    }
+    voice_label = st.selectbox("Choose Voice", list(voices[lang].keys()))
+
+    voice_id = voices[lang][voice_label]
+
+    async def generate_audio(text, voice):
+        communicate = edge_tts.Communicate(text=text, voice=voice)
+
+        audio_chunks = []
+        async for chunk in communicate.stream():
+            if chunk["type"] == "audio":
+                audio_chunks.append(chunk["data"])
+
+        return b"".join(audio_chunks)
+
+    if st.button("Convert"):
+        if text:
+            audio_bytes = asyncio.run(generate_audio(text, voice_id))
+            st.audio(audio_bytes, format="audio/mp3")
+        else:
+            st.warning("write your text first")
 
 else:
     st.header("🤖Chatbot")
